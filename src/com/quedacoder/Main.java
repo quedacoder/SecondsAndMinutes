76c3d384130a6310f708a5353292c55121072d6c
quedacoder <quedacoder@gmail.com>
2022-03-24 23:49:05 -0500

    Instructors version of the challenge. He used less code and i will need to work on using less not so complex solutions KISS

--- src/com/quedacoder/Main.java
+++ src/com/quedacoder/Main.java
@@ -2,7 +2,35 @@ package com.quedacoder;
 
 public class Main {
 
-    public static void main(String[] args) {
+    // Constants definitions
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";
+    private static final long VALUE_60_FOR_CALCULATION = 60L;
 
+    public static void main(String[] args) {
+        System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(3945L));
+        System.out.println(getDurationString(-41));
+        System.out.println(getDurationString(65, 9));
+    }
+
+    public static String getDurationString(long minutes, long seconds) {
+        if ((minutes < 0) || (seconds < 0) || (seconds > 59)) {
+            return INVALID_VALUE_MESSAGE;
+        }
+
+        long hours = minutes / VALUE_60_FOR_CALCULATION;
+        long remainingMinutes = minutes % VALUE_60_FOR_CALCULATION;
+        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+    }
+
+    public static String getDurationString(long seconds) {
+        if (seconds < 0) {
+            return INVALID_VALUE_MESSAGE;
+        }
+
+        long minutes = seconds / VALUE_60_FOR_CALCULATION;
+        long remainingSeconds = seconds % VALUE_60_FOR_CALCULATION;
+
+        return getDurationString(minutes, remainingSeconds);
     }
 }
